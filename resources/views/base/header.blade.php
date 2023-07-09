4d3eb75dc963f95a48ec5e73bfb2d5821c5d0a35
Lai Chun Ming <jimmy050311@gmail.com>
2023-07-09 13:01:13 +0800

    update

--- resources/views/base/header.blade.php
+++ resources/views/base/header.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <!-- <meta http-equiv="x-ua-compatible" content="ie=edge"> -->
     <title>袋鼠購物趣</title>
-    <meta name="robots" content="noindex, follow" />
+    <!-- <meta name="robots" content="noindex, follow" /> -->
     <meta name="description" content="袋鼠購物趣，挑戰市場最低價">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
